75df3b0ed7bfc9eceb53e2ccf0a7da095751e4bc
satyajit sahoo <55840143+satyajit501@users.noreply.github.com>
2023-02-25 03:45:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 import streamlit as st
-from PIL import Image
 
 
-st.title("My Curriculum Vitae")
-image = Image.open('home/jeet-mac/streamlit/proj_2/resources/images/mypic.jpg')
+st.title("My :red[Curriculum] Vitae")
+st.title("")
+st.markdown("Satyajit Sahoo (Data Science Intern)")
+st.markdown("Sankhachila, Jajpur road, Odisha")
+st.markdown("**Ph**- 7504799463")
+st.markdown("**Email**- ds.sahoosatyajit@gmail.com")
+st.markdown("**linkedin**- https://www.linkedin.com/in/satyajit-sahoo01/")
+st.markdown("**Objective:** To obtain a challenging position as a Data Scientist where I can utilize my skills and knowledge to contribute to the growth and success of the company.")
 
-st.image(image, caption='')
+st.markdown("**Work Experience:** Internship at Innomatics Research Labs(feb 2023): working on data cleaning and analysis tasks on various projects. Contributed to the development of a predictive model for customer retention using different types of algorithms.")
+
+st.markdown("**Programming languages:** Python, SQL")
+st.markdown("**Data analysis tools:** Excel, Tableau")
+st.markdown("**Machine learning algorithms:** Regression, Decision Trees, Random Forest, SVM, K-Means Clustering")
+
